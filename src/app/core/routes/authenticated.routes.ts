03c1d5f6f51894c72895bc907062f3d1aed4d3b6
Kirill Kamratov <kirill.kamratov@yandex.ru>
2024-10-16 00:32:08 +0300

    feat: add routes for users and feed

--- src/app/core/routes/authenticated.routes.ts
+++ src/app/core/routes/authenticated.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from "@angular/router";
 
-export const authenticatedRoutes: Routes = [];
+export const authenticatedRoutes: Routes = [
+  {
+    path: "feed",
+    loadChildren: () =>
+      import("../../news-feed/news-feed.routes").then((m) => m.newsFeedRoutes),
+  },
+  {
+    path: "user",
+    loadChildren: () =>
+      import("../../user/user.routes").then((m) => m.userRoutes),
+  },
+];
--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,1 +1,40 @@
-// user.routes.ts
+import { Routes } from "@angular/router";
+import { UserComponent } from "./user.component";
+import { UserWallComponent } from "./user-wall/user-wall.component";
+import { UserGalleryComponent } from "./user-gallery/user-gallery.component";
+import { UserPostComponent } from "./user-post/user-post.component";
+import { UserInfoComponent } from "./user-info/user-info.component";
+import { UserSettingsComponent } from "./user-settings/user-settings.component";
+
+export const userRoutes: Routes = [
+  {
+    path: "",
+    component: UserComponent,
+    children: [
+      {
+        path: ":id",
+        component: UserInfoComponent,
+        children: [
+          {
+            path: "wall",
+            component: UserWallComponent,
+            children: [
+              {
+                path: "post/:id",
+                component: UserPostComponent,
+              },
+            ],
+          },
+          {
+            path: "gallery",
+            component: UserGalleryComponent,
+          },
+          {
+            path: "settings",
+            component: UserSettingsComponent,
+          },
+        ],
+      },
+    ],
+  },
+];
