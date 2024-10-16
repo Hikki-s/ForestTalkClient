53915421f32f2f4cf0376e07168a23758140e0d0
Kirill Kamratov <kirill.kamratov@yandex.ru>
2024-10-17 00:27:23 +0300

    feat: add groups and single group routes

--- src/app/groups/groups.component.ts
+++ src/app/groups/groups.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: "app-groups",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: "./groups.component.html",
   styleUrl: "./groups.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
