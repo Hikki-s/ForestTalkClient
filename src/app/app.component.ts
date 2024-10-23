779efd703df0a039d133feebc4e039016bc1d988
Hikki-s <bogdanpleshkov2002@gmail.com>
2024-10-23 22:24:35 +0300

    fix(taiga-ui): hotfix dependency issue and linting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TuiRootModule } from "@taiga-ui/core";
 
-
 @Component({
   standalone: true,
   selector: "app-root",
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,12 @@
 import { NG_EVENT_PLUGINS } from "@taiga-ui/event-plugins";
 import { provideAnimations } from "@angular/platform-browser/animations";
-import {ApplicationConfig, importProvidersFrom} from "@angular/core";
+import type { ApplicationConfig } from "@angular/core";
+import { importProvidersFrom } from "@angular/core";
 import { provideZoneChangeDetection } from "@angular/core";
 import { provideRouter } from "@angular/router";
 import { provideHttpClient } from "@angular/common/http";
+import { TuiRootModule } from "@taiga-ui/core";
 import { appRoutes } from "./app.routes";
-import {TuiRootModule} from "@taiga-ui/core";
 
 export const appConfig: ApplicationConfig = {
   providers: [
