848cd053e84874994b5c7ac81ebc8f8fb75cd5d5
Hikki-s <bogdanpleshkov2002@gmail.com>
2024-10-28 15:16:45 +0300

    fix: linting, fix adaptive styles and add forest.svg on background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from "@angular/core";
 import { RouterOutlet } from "@angular/router";
 import { TuiRootModule } from "@taiga-ui/core";
+import { TuiIconModule } from "@taiga-ui/experimental";
 
 @Component({
   standalone: true,
   selector: "app-root",
   templateUrl: "app.component.html",
   styleUrl: "app.component.less",
-  imports: [TuiRootModule, RouterOutlet],
+  imports: [TuiRootModule, RouterOutlet, TuiIconModule],
 })
 export class AppComponent {}
