d320cef8aa3c37b946d60b24a2fff51467eda7f8
Hikki-s <bogdanpleshkov2002@gmail.com>
2024-10-29 17:28:28 +0300

    feat(sidebar): add sidebar component and add styles for sidebar

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,36 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { TuiCardModule, TuiSurfaceModule } from "@taiga-ui/experimental";
+import {
+  BookUser,
+  LucideAngularModule,
+  MessageSquare,
+  Newspaper,
+  SquareUserRound,
+  UsersRound,
+} from "lucide-angular";
+import { RouterLink } from "@angular/router";
+import { TuiLinkModule } from "@taiga-ui/core";
 
 @Component({
   selector: "app-sidebar",
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TuiCardModule,
+    TuiSurfaceModule,
+    LucideAngularModule,
+    RouterLink,
+    TuiLinkModule,
+  ],
   templateUrl: "./sidebar.component.html",
   styleUrl: "./sidebar.component.less",
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  protected readonly Newspaper = Newspaper;
+  protected readonly SquareUserRound = SquareUserRound;
+  protected readonly MessageSquare = MessageSquare;
+  protected readonly UsersRound = UsersRound;
+  protected readonly BookUser = BookUser;
+}
