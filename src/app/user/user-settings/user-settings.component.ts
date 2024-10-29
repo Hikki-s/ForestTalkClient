b35297fbc7953a13c1ee5ff46c03ea99de0ff019
Павел Журавлев <pavelzuravlev@MacBook-Pro-Pavel.local>
2024-10-29 11:42:40 +0300

    fix: fix formatter

--- src/app/user/user-settings/user-settings.component.ts
+++ src/app/user/user-settings/user-settings.component.ts
@@ -1,8 +1,8 @@
 import { ChangeDetectionStrategy, Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { TuiInputModule } from '@taiga-ui/kit';
-import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
-import {TuiButtonModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
+import { TuiInputModule } from "@taiga-ui/kit";
+import { ReactiveFormsModule, FormControl, FormGroup } from "@angular/forms";
+import { TuiButtonModule, TuiTextfieldControllerModule } from "@taiga-ui/core";
 
 @Component({
   selector: "app-user-settings",
@@ -12,7 +12,7 @@ import {TuiButtonModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
     TuiInputModule,
     ReactiveFormsModule,
     TuiTextfieldControllerModule,
-    TuiButtonModule
+    TuiButtonModule,
   ],
   templateUrl: "./user-settings.component.html",
   styleUrls: ["./user-settings.component.less"],
@@ -20,24 +20,40 @@ import {TuiButtonModule, TuiTextfieldControllerModule} from "@taiga-ui/core";
 })
 export class UserSettingsComponent {
   readonly userForm = new FormGroup({
-    firstName: new FormControl(''),
-    lastName: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    birthDate: new FormControl(''),
-    workPlace: new FormControl(''),
-    residence: new FormControl(''),
-    language: new FormControl(''),
+    firstName: new FormControl(""),
+    lastName: new FormControl(""),
+    email: new FormControl(""),
+    phone: new FormControl(""),
+    birthDate: new FormControl(""),
+    workPlace: new FormControl(""),
+    residence: new FormControl(""),
+    language: new FormControl(""),
   });
 
   fields = [
-    { label: 'Имя', controlName: 'firstName', placeholder: 'Введите имя' },
-    { label: 'Фамилия', controlName: 'lastName', placeholder: 'Введите фамилию' },
-    { label: 'Почта', controlName: 'email', placeholder: 'Введите почту' },
-    { label: 'Телефон', controlName: 'phone', placeholder: 'Введите телефон' },
-    { label: 'Дата рождения', controlName: 'birthDate', placeholder: 'Введите дату рождения' },
-    { label: 'Место работы', controlName: 'workPlace', placeholder: 'Введите место работы' },
-    { label: 'Место проживания', controlName: 'residence', placeholder: 'Введите место проживания' },
-    { label: 'Язык', controlName: 'language', placeholder: 'Введите язык' },
+    { label: "Имя", controlName: "firstName", placeholder: "Введите имя" },
+    {
+      label: "Фамилия",
+      controlName: "lastName",
+      placeholder: "Введите фамилию",
+    },
+    { label: "Почта", controlName: "email", placeholder: "Введите почту" },
+    { label: "Телефон", controlName: "phone", placeholder: "Введите телефон" },
+    {
+      label: "Дата рождения",
+      controlName: "birthDate",
+      placeholder: "Введите дату рождения",
+    },
+    {
+      label: "Место работы",
+      controlName: "workPlace",
+      placeholder: "Введите место работы",
+    },
+    {
+      label: "Место проживания",
+      controlName: "residence",
+      placeholder: "Введите место проживания",
+    },
+    { label: "Язык", controlName: "language", placeholder: "Введите язык" },
   ];
 }
